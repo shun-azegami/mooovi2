b9cb5e3386e909b50c844b0da7007dfd30a60f97
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-07-31 23:58:44 +0900

    個別ページ〜検索ページの表示実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,2 @@
-class ReviewsController < RankingController
+class ReviewsController < ApplicationController
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 TechReviewSite::Application.routes.draw do
+
+  get 'products' => 'products#index'
+  get 'products/search' => 'products#search'
+  get 'products/:id' => 'products#show'
+
+  root 'products#index'
+
 end
