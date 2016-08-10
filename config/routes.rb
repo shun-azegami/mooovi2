e86508f6b418cc7ddce9ad450b8950d7144de906
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-08-10 23:36:10 +0900

    nest

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 TechReviewSite::Application.routes.draw do
 
-  get 'products' => 'products#index'
-  get 'products/search' => 'products#search'
-  get 'products/:id' => 'products#show'
-  get 'products/:product_id/reviews/new' => 'reviews#new'
-  post 'products/:product_id/reviews' => 'reviews#create'
-
+  resources :products, only: :show do
+    resources :reviews, only: [:new, :create]
+    collection do
+      get 'search'
+    end
+  end
   root 'products#index'
 
 end
